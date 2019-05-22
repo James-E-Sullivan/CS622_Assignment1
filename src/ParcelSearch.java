1fb8c34b853f25b427f204785e1168166859a3d7
James Sullivan <jesullivan222@gmail.com>
2019-05-21 20:44:43 -0400

    Updated ParcelSearch. Not functioning properly. Will scale back search function for Module 1 release.

--- src/ParcelSearch.java
+++ src/ParcelSearch.java
@@ -8,48 +8,6 @@ import java.util.Scanner;
 
 public class ParcelSearch {
 
-    /*
-    private static void chooseParameters(int landUse, SearchParameters userParameters){
-
-        boolean done = false;
-        while (!done){
-            if(input.equals("1") | input.toLowerCase().equals("commercial")){
-                System.out.println("Please choose one of the following parameters to narrow your search." +
-                        " Otherwise, enter 'continue'.");
-                System.out.println("    1. Parcel ID");
-                System.out.println("    2. Address");
-                System.out.println("    3. Property Value");
-                System.out.println("    4. Land Area");
-                input = scan.nextLine();
-
-                if(input.equals("1") | input.toLowerCase().equals("parcel id")){
-                    System.out.println("Enter the desired Parcel ID: ");
-                    userParameters.setParcelID(scan.nextLine());    // does not check for validity
-                }
-                else if(input.equals("2") | input.toLowerCase().equals("address")){
-                    System.out.println("Enter an address: ");
-                    userParameters.setAddress(scan.nextLine());
-                }
-                else if(input.equals("3") | input.toLowerCase().equals("property value")){
-                    System.out.println("Enter the lower bound property value (inclusive): ");
-                    userParameters.setPropertyValueLower(Integer.valueOf(scan.nextLine()));
-                    System.out.println("Enter the upper bound property value (inclusive): ");
-                    userParameters.setPropertyValueUpper(Integer.valueOf(scan.nextLine()));
-                }
-                else if(input.equals("4") | input.toLowerCase().equals("land area")){
-                    System.out.println("Enter the lower bound of land area (inclusive): ");
-                    userParameters.setLandAreaLower(Integer.valueOf(scan.nextLine()));
-                    System.out.println("Enter the upper bound of land area (inclusive): ");
-                    userParameters.setLandAreaUpper(Integer.valueOf(scan.nextLine()));
-                }
-                else{
-                    System.out.println("Invalid entry.");
-                }
-        }
-    }
-
-     */
-
     public static void executeSearch(SearchParameters param){
 
     }
@@ -59,10 +17,6 @@ public class ParcelSearch {
         CSV_Input bostonParcels = new CSV_Input();
         bostonParcels.read_CSV_File();              // reads commercial and residential parcel info into HashMaps
         HashMap<String, ResidentialParcel> bostonResidentialMap = bostonParcels.getResidentialMap();
-
-
-
-
         HashMap<String, CommercialParcel> bostonCommercialMap = bostonParcels.getCommercialMap();
 
         Scanner scan = new Scanner(System.in);
@@ -151,8 +105,6 @@ public class ParcelSearch {
                 catch (IllegalArgumentException ex){
                     System.err.println("Caught IllegalArgumentException: " + ex.getMessage());
                 }
-
-
             }
 
         done = true;
@@ -161,12 +113,32 @@ public class ParcelSearch {
 
         if(userParameters.getLandUseType().equals("Residential")){
             for(ResidentialParcel rp : bostonResidentialMap.values()){
+
+                if((userParameters.getParcelID() != null) &&
+                        !(rp.getParcelID().equals(userParameters.getParcelID()))){
+                    continue;
+                }
+                else if((userParameters.getAddress() != null) &&
+                        !(rp.getAddress().toLowerCase().contains(userParameters.getAddress().toLowerCase()))){
+                    continue;
+                }
+
                 System.out.println();
                 rp.display();
             }
         }
         else if(userParameters.getLandUseType().equals("Commercial")){
             for(CommercialParcel cp : bostonCommercialMap.values()){
+
+                if((userParameters.getParcelID() != null) &&
+                        !(cp.getParcelID().equals(userParameters.getParcelID()))){
+                    continue;
+                }
+                else if((userParameters.getAddress() != null) &&
+                        !(cp.getAddress().toLowerCase().contains(userParameters.getAddress().toLowerCase()))){
+                    continue;
+                }
+
                 System.out.println();
                 cp.display();
             }
