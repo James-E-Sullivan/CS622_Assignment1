c717bc8e8f942e5941eaa316cfead4108d13be8a
James Sullivan <jesullivan222@gmail.com>
2019-05-25 10:32:13 -0400

    Completed writeSearchResults and overwriteSearchResults.

--- src/output/SearchOutput.java
+++ src/output/SearchOutput.java
@@ -6,14 +6,27 @@ public class SearchOutput {
 
     final static String OUTPUT_FILE = "resources/Search_Result_Output";
 
-    public static void writeSearchResults(String results) throws IOException {
+    /**
+     * Appends String of search results to OUTPUT_FILE
+     * @param results: String of user search results
+     * @throws IOException: Throws IOException to caller
+     */
+    public void writeSearchResults(String results) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(OUTPUT_FILE, true));   // append to file
+        out.println("\n" + results);
+        out.close();
+    }
+
+    /**
+     * Overwrites OUTPUT_FILE with String of search results.
+     * @param results: String of user search results.
+     * @throws IOException: Throws IOException to caller
+     */
+    public void overwriteSearchResults(String results) throws IOException {
         PrintWriter out = new PrintWriter(new FileWriter(OUTPUT_FILE));
         out.println(results);
         out.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        writeSearchResults("Test1");
-    }
 
 }
